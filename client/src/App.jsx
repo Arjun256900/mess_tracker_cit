530a53e0e303bf43c2714791296061625f44eb1f
Arjun <alphasva1998@gmail.com>
2024-08-03 22:06:00 +0530

    Separattion of client and server logics and server setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar.jsx";
+import Login from "./components/Login.jsx";
+function App() {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
