eee0448a9312c268d86a5ff922d3c095ba42f9e4
Arjun <alphasva1998@gmail.com>
2024-08-03 13:06:18 +0530

    Login and register pages included

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/Navbar.jsx";
+import Login from "./components/Login.jsx";
 function App() {
-
   return (
-    <div >
-        <Navbar/>
+    <div>
+      {/* <Navbar/> */}
+      <Login />
     </div>
   );
 }
 
 export default App;
-  
